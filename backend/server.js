45ae204d850aa55ec30617555972ffd04c22154a
Henry B <noelys215@gmail.com>
2022-12-11 16:27:42 -0500

    fix

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const orderRoutes = require('./routes/orderRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const cors = require('cors');
+const path = require('path');
 
 const cloudinary = require('cloudinary');
 
@@ -24,17 +25,27 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.send('API RUNNING');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, '..', 'frontend', 'build', 'server', 'pages', 'index.html')
+		);
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.send('API RUNNING');
+	});
+}
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+	distDir: 'build',
 	reactStrictMode: false,
 	images: {
 		domains: ['cdn.sanity.io', 'localhost', '127.0.0.1', 'res.cloudinary.com'],
 		loader: 'default',
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: 'https://api.example.com/:path*',
-			},
-		];
 	},
 };
